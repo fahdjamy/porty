1d44d68c0656031e03e446cd87dc0942ea762365
fahdjamy <fahdjamy2@gmail.com>
2020-02-02 22:54:12 +0300

    add routing to the project

---
+++ frontend/src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>About</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  a {
+    color: #42b983;
+  }
+</style>
